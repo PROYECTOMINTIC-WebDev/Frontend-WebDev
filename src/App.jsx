c649fa602c0fcea4d730bc4f898c4cffca43a809
floza97 <florezdiego007@gmail.com>
2021-11-22 21:56:04 -0500

    actualizacion de version router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Registro from './pages/Registro';
+import Admin from './pages/Admin';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
+
 
 function App() {
   return (
-    <div>hola</div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/admin" element={<Admin />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
