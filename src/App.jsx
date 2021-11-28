146d91b3363b2d2befe5266cf8f35de275c95612
floza97 <florezdiego007@gmail.com>
2021-11-28 17:21:27 -0500

    creacion interfaz de registro,components,styles,utils,

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import Index from "./pages/index/Index";
 import Login from "./pages/login/Login";
-import Registro from "./pages/registro/Registro";
 import Admin from "./pages/admin/Admin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import './styles/globals.css';
 import Sidebar from "./modules/sidebar/sidebar";
-
 import Adminusers from "./pages/admin/Adminusers";
 import PrivateLayout from "./layout/PrivateLayout";
+import AuthLayout from "./layout/AuthLayout";
+import Registro from "./pages/auth/Registro";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      //ruta privada de administrador
         <Route path="/" element={<PrivateLayout />}>
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/usuarios" element={<Adminusers />} />
-
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/usuarios" element={<Adminusers />} />
         </Route>
+
         <Route path="" element={<Index />} />
-       
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/sidebar" element={<Sidebar />} />
+      //ruta de autenticacion de registro
+          <Route path='/auth' element={<AuthLayout />}>
+            <Route path='registro' element={<Registro />} />
+          </Route>
+
       </Routes>
     </BrowserRouter>
   );
